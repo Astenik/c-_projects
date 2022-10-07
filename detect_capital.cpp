7e1058dc9b3ea46f675eb8577bc317913025d686
Astenik <106985461+Astenik@users.noreply.github.com>
2022-10-07 19:44:22 +0400

    Update detect_capital.cpp

--- detect_capital.cpp
+++ detect_capital.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <assert.h>
-using namespace std;
+
 bool detect_capital(char* name, int size)
 {
     if(name[0] >= 'A' && name[0] <= 'Z')
@@ -11,19 +11,23 @@ bool detect_capital(char* name, int size)
             if(name[i] >= 'a' && name[i] <= 'z') {  continue; }
             else { break; }
         }
+        
         if(i == size){  return true; }
         for(j = 1; j < size; ++j)
         {
             if(name[j] >= 'A' && name[j] <= 'Z') {  continue; }
             else { break; }
         }
+        
         if(j == size) { return true; }
         else { return false; }
     }
+    
     for(int k = 0; k < size; ++k)
     {
         if(name[k] >= 'A' && name[k] <= 'Z') { return false; }
         
     }
+    
     return true;
 }
