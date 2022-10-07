9109e1e17daff16c79ac511d4299bda7934cf54c
Astenik <106985461+Astenik@users.noreply.github.com>
2022-10-07 19:57:25 +0400

    Update intersection_of_arrays.cpp

--- intersection_of_arrays.cpp
+++ intersection_of_arrays.cpp
@@ -1,32 +1,44 @@
 #include <iostream>
 #include <assert.h>
-using namespace std;
+
 void intersection_of_arrays(int*, int*, int, int);
+
 int main()
-{   int size1, size2;
-     cout<<"input size1"<<endl;
-     cin>>size1;
-     cout<<"input size2"<<endl;
-     cin>>size2;
+{  
+     int size1, size2;
+     
+     std::cout << "input size1" << std::endl;
+     std::cin >> size1;
+     
+     std::cout << "input size2" << std::endl;
+     std::cin >> size2;
+     
     int* nums1 = new int [size1];
     assert(nums1);
+     
     int* nums2 = new int [size2];
     assert(nums2);
+     
      for(int i = 0; i < size1; ++i)
      {
-         cout<<"input the member of nums1";
-         cin>>nums1[i];
+         std::cout << "input the member of nums1";
+         std::cin >> nums1[i];
      }
+     
      for(int i = 0; i < size2; ++i)
      {
-         cout<<"input the member of nums2";
-         cin>>nums2[i];
+         std::cout << "input the member of nums2";
+         std::cin >> nums2[i];
      }
+     
      intersection_of_arrays(nums1, nums2, size1, size2);
+     
      delete [] nums1;
      delete [] nums2;
+     
      return 0;
 }
+
 void intersection_of_arrays(int* v1, int* v2, int s1, int s2)
 {
     int s = s1 > s2 ? s2 : s1;
@@ -34,3 +46,23 @@ void intersection_of_arrays(int* v1, int* v2, int s1, int s2)
     int q = 0;
     for(int i = 0; i < s1; ++i)
     {
+       for(int j = 0; i < s2; ++j)
+       {
+           if(v1[i] == v2[j])
+           {
+                v[q] = v1[i];
+                ++q;
+           }
+       }
+    }
+}
+            
+           
+           
+           
+           
+           
+           
+           
+           
+           
