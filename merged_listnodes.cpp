e31f127c12e255745538e4601f7d6327aae5375b
Astenik <106985461+Astenik@users.noreply.github.com>
2022-10-07 19:34:43 +0400

    Update and rename merged_listnodes.C++ to merged_listnodes.cpp

--- merged_listnodes.cpp
+++ merged_listnodes.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <assert.h>
-using namespace std;
+
 class ListNode
 {
   public:
@@ -11,11 +11,13 @@ class ListNode
      ListNode(int val, ListNode* n): value(val), next(n){}
     ~ListNode(){ delete next; }
 };
+
 void merge_lists(ListNode* nums1, ListNode* nums2, int s1, int s2)
 {
     int s = s1 + s2;
     int k = 0, i, j;
     ListNode* nums = new ListNode [s];
+  
     for(i = 0; i < s1; )
     {
         for(j = 0; j < s2; )
@@ -51,6 +53,7 @@ void merge_lists(ListNode* nums1, ListNode* nums2, int s1, int s2)
             ++k;
         }
     }
+  
     for(int d = 0; d < s - 1; ++d)
     {
         nums[d].next = &nums[d + 1];
@@ -58,23 +61,29 @@ void merge_lists(ListNode* nums1, ListNode* nums2, int s1, int s2)
     nums[s - 1].next = nullptr;
     for(int d = 0; d < s; ++d)
     {
-        cout<<nums[d].value<<" ";
+        std::cout << nums[d].value << " ";
     }
 }
+
 int main()
 {
     ListNode* n1 = new ListNode [3];
     ListNode* n2 = new ListNode [4];
     for(int i = 0; i < 3; ++i)
-         cin>> n1[i].value;
+         std::cin >> n1[i].value;
+  
     for(int i = 0; i < 4; ++i)
-         cin>> n2[i].value;
+         std::cin >> n2[i].value;
+  
     for(int i = 0; i < 2; ++i)
          n1[i].next = &n1[i+1];
     n1[2].next = nullptr;
+  
     for(int i = 0; i < 3; ++i)
          n2[i].next = &n2[i+1];
     n2[3].next = nullptr;
+  
     merge_lists(n1, n2, 3, 4);
+  
     return 0;
 }
